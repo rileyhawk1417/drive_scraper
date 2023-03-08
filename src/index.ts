d4110e3c4872a2714427626f63dc314e36c30a9d
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-03-08 20:08:33 +0200

    feat: API sends back data

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,54 @@ const CORS = cors()
 const app: Express = express()
 const PORT = process.env.PORT || 5000
 
-const res = await GetGDrive(couples, true)
-DB.put('key', { key: res })
-let reply = DB.get('key').key
 app.use(CORS)
+app.use(express.json())
 app.get('/data', (req: Request, res: Response) => {
-  res.send(reply)
+  let reply = loadData()
+  res.setHeader('Content-type', 'application/json')
+  res.send(JSON.stringify(reply))
 })
+
+// Include function  to post
+app.post('/fetch', async (req, res) => {
+  let results: any = {}
+  try {
+    let query = req.body
+    await getURL(query.url)
+    let data = await loadData()
+    results.data = data
+    results.success = true
+  } catch (e) {
+    console.log(e)
+  } finally {
+    res.setHeader('Content-type', 'application/json')
+    res.send(JSON.stringify(results))
+  }
+}
+
+)
 
 app.listen(PORT, () => {
   console.log(`Server runnning on PORT: ${PORT}`)
 })
 
+const getURL = async (url: string) => {
+  try {
+    const res = await GetGDrive(url, true)
+    DB.put('key', { key: res })
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+
+const loadData = async () => {
+  try {
+    let reply = DB.get('key').key
+    return reply
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
 //
