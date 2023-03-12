58eae71e22bb468a8202beb1e819c3395998e359
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-03-12 14:06:32 +0200

    chore: update vercel config

---
+++ index.ts
@@ -0,0 +1,36 @@
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors'
+import GetGDrive from './routes/api.js'
+import { open } from 'lmdb';
+import sharp from 'sharp'
+import { ReturnData, QueryData } from './routes/fetch.js'
+
+const DB = open({ path: 'img-db', compression: true })
+const couples = 'https://drive.google.com/drive/folders/1uwxeLdxCGsBmMOgTue9qiKz2iFu3hWv9?usp=sharing'
+const CORS = cors()
+const app: Express = express()
+const PORT = process.env.PORT || 5000
+
+const handler = (res, req) => {
+  res.status(200).end('Server OK')
+}
+
+app.use(CORS)
+app.use(express.json())
+app.get('/data', async (req: Request, res: Response) => {
+  ReturnData(res, req)
+})
+
+// Include function  to post
+app.post('/fetch', async (req, res) => {
+  QueryData(res, req)
+}
+
+)
+
+app.listen(PORT, () => {
+  console.log('Server is up and runnning :)')
+})
+
+
+export default handler
--- routes/fetch.ts
+++ routes/fetch.ts
@@ -1,58 +1,6 @@
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors'
 import GetGDrive from './api.js'
-import { open } from 'lmdb';
-import sharp from 'sharp'
-import { encode, decode } from 'blurhash'
-
+import { open } from 'lmdb'
 const DB = open({ path: 'img-db', compression: true })
-const couples = 'https://drive.google.com/drive/folders/1uwxeLdxCGsBmMOgTue9qiKz2iFu3hWv9?usp=sharing'
-const CORS = cors()
-const app: Express = express()
-const PORT = process.env.PORT || 5000
-
-const handler = (res, req) => {
-  res.status(200).end('Server OK')
-}
-
-app.use(CORS)
-app.use(express.json())
-app.get('/data', async (req: Request, res: Response) => {
-  try {
-    let reply = await loadData()
-    res.statusCode = 200
-    res.setHeader('Content-type', 'application/json')
-    res.send(JSON.stringify(reply))
-  } catch (e) {
-    res.statusCode = 500
-    res.send(JSON.stringify('Failed to fetch data'))
-  }
-})
-
-// Include function  to post
-app.post('/fetch', async (req, res) => {
-  let results: any = {}
-  try {
-    let query = req.body
-    await getURL(query.url)
-    let data = await loadData()
-    results.data = data
-    results.success = true
-  } catch (e) {
-    console.log(e)
-    res.statusCode = 500
-  } finally {
-    res.statusCode = 200
-    res.setHeader('Content-type', 'application/json')
-    res.send(JSON.stringify(results))
-  }
-}
-
-)
-
-app.listen(PORT, () => {
-  console.log('Server is up and runnning :)')
-})
 
 let reg = new RegExp(/['*.jpg', '*.png']/g)
 
@@ -119,4 +67,32 @@ const loadData = async () => {
   }
 }
 
-export default handler
+export const ReturnData = async (res: any, req: any) => {
+  try {
+    let reply = await loadData()
+    res.statusCode = 200
+    res.setHeader('Content-type', 'application/json')
+    res.send(JSON.stringify(reply))
+  } catch (e) {
+    res.statusCode = 500
+    res.send(JSON.stringify('Failed to fetch data'))
+  }
+}
+
+export const QueryData = async (res: any, req: any) => {
+  let results: any = {}
+  try {
+    let query = req.body
+    await getURL(query.url)
+    let data = await loadData()
+    results.data = data
+    results.success = true
+  } catch (e) {
+    console.log(e)
+    res.statusCode = 500
+  } finally {
+    res.statusCode = 200
+    res.setHeader('Content-type', 'application/json')
+    res.send(JSON.stringify(results))
+  }
+}
